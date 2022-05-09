cdad2c8d411c0439dd77e251d0352f694495d08f
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2022-05-09 11:31:31 +0200

    Hide tag group if there are none of that type

--- src/Components/Articles/ArticleTags.tsx
+++ src/Components/Articles/ArticleTags.tsx
@@ -10,42 +10,53 @@ export default function ArticleTags({ tags }: ArticleTagsProps) {
             id="tags"
             className="d-flex flex-md-row flex-column justify-content-md-around flex-wrap"
         >
-            <div className="d-flex">
-                <span className="font-weight-bold">Klasse:</span>
-                &nbsp;
-                {tags
-                    .filter((x) => x.tagType === "grade")
-                    .map((tag) => (
-                        <span key={tag.id}>{tag.name}</span>
-                    ))}
-            </div>
-            <div className="d-flex">
-                <span className="font-weight-bold">Fag:</span>
-                &nbsp;
-                {tags
-                    .filter((x) => x.tagType === "subject")
-                    .map((tag) => (
-                        <span key={tag.id}>{tag.name}</span>
-                    ))}
-            </div>
-            <div className="d-flex">
-                <span className="font-weight-bold">Tema:</span>
-                &nbsp;
-                {tags
-                    .filter((x) => x.tagType === "theme")
-                    .map((tag) => (
-                        <span key={tag.id}>{tag.name}</span>
-                    ))}
-            </div>
-            <div className="d-flex">
-                <span className="font-weight-bold">Verktøy:</span>
-                &nbsp;
-                {tags
-                    .filter((x) => x.tagType === "tool")
-                    .map((tag) => (
-                        <span key={tag.id}>{tag.name}</span>
-                    ))}
-            </div>
+            {tags.filter((x) => x.tagType === "grade").length === 0 ? null : (
+                <div className="d-flex">
+                    <span className="font-weight-bold">Klasse:</span>
+                    &nbsp;
+                    {tags
+                        .filter((x) => x.tagType === "grade")
+                        .map((tag) => (
+                            <span key={tag.id}>{tag.name}</span>
+                        ))}
+                </div>
+            )}
+
+            {tags.filter((x) => x.tagType === "subject").length === 0 ? null : (
+                <div className="d-flex">
+                    <span className="font-weight-bold">Fag:</span>
+                    &nbsp;
+                    {tags
+                        .filter((x) => x.tagType === "subject")
+                        .map((tag) => (
+                            <span key={tag.id}>{tag.name}</span>
+                        ))}
+                </div>
+            )}
+
+            {tags.filter((x) => x.tagType === "theme").length === 0 ? null : (
+                <div className="d-flex">
+                    <span className="font-weight-bold">Tema:</span>
+                    &nbsp;
+                    {tags
+                        .filter((x) => x.tagType === "theme")
+                        .map((tag) => (
+                            <span key={tag.id}>{tag.name}</span>
+                        ))}
+                </div>
+            )}
+
+            {tags.filter((x) => x.tagType === "tool").length === 0 ? null : (
+                <div className="d-flex">
+                    <span className="font-weight-bold">Verktøy:</span>
+                    &nbsp;
+                    {tags
+                        .filter((x) => x.tagType === "tool")
+                        .map((tag) => (
+                            <span key={tag.id}>{tag.name}</span>
+                        ))}
+                </div>
+            )}
         </div>
     );
 }
